d165c0b53fd3a858b9a2871396e669f51044f8e1
Ali-Rooholamini <ali.rooholamini.dev@gmail.com>
2023-11-22 15:04:53 +0330

    feat: add image-placeholder to fast-food-item componenet

--- src/FastFoodItem/fastFoodItem.jsx
+++ src/FastFoodItem/fastFoodItem.jsx
@@ -1,3 +1,5 @@
+import "./fastFoodItem.css";
+
 import { HiShoppingCart } from "react-icons/hi";
 
 const FastFoodItem = ({ name, price, ingredients, imageUrl }) => {
@@ -6,7 +8,9 @@ const FastFoodItem = ({ name, price, ingredients, imageUrl }) => {
       <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
         فیمت : {price.toLocaleString()} تومان
       </span>
-      <img className="card-img-top" src={imageUrl} alt={name} />
+      <div className="card__placeholder">
+        <img className="card-img-top" src={imageUrl} alt={name} />
+      </div>
       <div className="card-body d-flex flex-column text-center pt-3 pb-4">
         <h5 className="mb-2">{name}</h5>
         <p className="text-muted fs-ms fw-bold mb-3">{ingredients}</p>
